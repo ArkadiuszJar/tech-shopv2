e48468b262c6d058e034da108495343a3cb4684b
Arek Jarnecki <45920852+ArkadiuszJar@users.noreply.github.com>
2023-02-28 13:13:32 +0100

    fix: replace Match.randown with CSPRNG form safety

--- components/homeLoader.tsx
+++ components/homeLoader.tsx
@@ -1,12 +1,14 @@
 import ItemLoader from "./itemLoader";
 
+var rand = require("csprng");
+
 const HomeLoader = () => {
 	return (
 		<div className="flex flex-wrap lg:w-2/3 sm:w-full justify-center">
 			{Array(5)
 				.fill(0)
-				.map((i: number) => (
-					<ItemLoader key={Math.random()} />
+				.map(() => (
+					<ItemLoader key={rand(160, 36)} />
 				))}
 		</div>
 	);
