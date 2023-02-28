167648f1f8d710a286b19429423b2166585317f0
Arek Jarnecki <45920852+ArkadiuszJar@users.noreply.github.com>
2023-02-28 12:51:08 +0100

    fix: set unique key for each home itemloader

--- components/homeLoader.tsx
+++ components/homeLoader.tsx
@@ -5,8 +5,8 @@ const HomeLoader = () => {
 		<div className="flex flex-wrap lg:w-2/3 sm:w-full justify-center">
 			{Array(5)
 				.fill(0)
-				.map(() => (
-					<ItemLoader key={1} />
+				.map((i: number) => (
+					<ItemLoader key={Math.random()} />
 				))}
 		</div>
 	);
